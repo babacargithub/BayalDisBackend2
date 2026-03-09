95102920891919735121de7fed52338299e1eb89
babacargithub <46243872+babacargithub@users.noreply.github.com>
2026-03-09 18:14:57 +0000

    Added Warehouse feature in the app

--- app/Models/StockEntry.php
+++ app/Models/StockEntry.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StockEntry extends Model
 {
     use HasFactory;
-    protected $fillable = ['product_id', 'quantity', 'quantity_left',"purchase_invoice_item_id",
-    "unit_price",];
+
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'quantity_left',
+        'purchase_invoice_item_id',
+        'unit_price',
+        'warehouse_id',
+    ];
+
+    public function warehouse(): BelongsTo
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
 }
---
+++ app/Models/Warehouse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Warehouse extends Model
+{
+    /** @use HasFactory<\Database\Factories\WarehouseFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'manager_id',
+    ];
+
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'manager_id');
+    }
+
+    public function stockEntries(): HasMany
+    {
+        return $this->hasMany(StockEntry::class);
+    }
+}
